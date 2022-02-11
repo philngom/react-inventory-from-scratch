f57ca5fd9f4735695283a52754c21a799b51f733
Philippe Ngom <philngom@gmail.com>
2022-02-11 00:09:41 -0800

    finished MovieListPage

--- src/MovieListPage.js
+++ src/MovieListPage.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { getMovies } from './services/fetch-utils.js';
+import Movie from './Movie';
+
 export default function MovieListPage() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const moviesData = await getMovies();
+      setMovies(moviesData);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <div>
-      in movie list page
+      {
+        movies.map((movie, i) =>
+          <Movie key={movie + i} movie={movie}/>
+        )
+      }
     </div>
   );
 }
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -32,6 +32,5 @@ export async function getMovies() {
   const response = await client
     .from('movies')
     .select();
-
   return checkError(response);
 }
