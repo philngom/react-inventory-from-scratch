43a01ed100048369bc14970d1784ff0ba0b98770
Philippe Ngom <philngom@gmail.com>
2022-02-11 09:33:34 -0800

    added form to create page

--- src/CreateMovie.js
+++ src/CreateMovie.js
@@ -2,10 +2,33 @@ import { useState } from 'react';
 
 export default function CreateMovie() {
 
+  const [name, setName] = useState('');
+  const [director, setDirector] = useState('');
+  const [mainCharacter, setMainCharacter] = useState('');
+  const [year, setYear] = useState(null);
+
 
   return (
     <div>
-      we in create movie
+      <form>
+        <label>
+          Title:
+          <input value={name} onChange={e => setName(e.target.value)}/>
+        </label>
+        <label>
+          Year:
+          <input value={year} onChange={(e) => setYear(e.target.value)}/>
+        </label>
+        <label>
+          Director:
+          <input value={director} onChange={(e) => setDirector(e.target.value)}/>
+        </label>
+        <label>
+          Main Character:
+          <input value={mainCharacter} onChange={(e) => setMainCharacter(e.target.value)}/>
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
