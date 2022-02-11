830c80f076dc62c95cc1fce1fdb26501a68f0034
Philippe Ngom <philngom@gmail.com>
2022-02-10 21:25:05 -0800

    added signIn, signOut and getUser functions to fetch-utils

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,2 +1,23 @@
 import { client, checkError } from './client.js';
 
+export function getUser() {
+  return client.auth.session();
+}
+
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({
+    email: email,
+    password: password,
+  });
+
+  return response.user;
+}
+
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({
+    email: email,
+    password: password,
+  });
+
+  return response.user;
+}
